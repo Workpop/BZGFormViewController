bbb9b344588f5ac6b2168c60d0ed7f9eef2e03ec
Seth Sandler <cerupcat@gmail.com>
2015-07-22 16:37:22 -0700

    Create initial BZGRichTextViewCell

---
+++ BZGFormViewController/BZGRichTextViewCell.h
@@ -0,0 +1,69 @@
+//
+//  BZGTextFieldCell.h
+//
+//  https://github.com/benzguo/BZGFormViewController
+//
+
+#import <UIKit/UIKit.h>
+#import <JVFloatLabeledTextField/JVFloatLabeledTextView.h>
+#import "BZGFormCell.h"
+
+#import "ZSSRichTextEditor.h"
+
+@interface BZGRichTextViewCell : BZGFormCell
+
+@property (strong, nonatomic) UILabel *label;
+
+@property (strong, nonatomic) UITextView *textField;
+
+/// The color of the text field's text when the cell's state is not invalid.
+@property (strong, nonatomic) UIColor *textFieldNormalColor;
+
+/// The color of the text field's text when the cell's state is invalid.
+@property (strong, nonatomic) UIColor *textFieldInvalidColor;
+
+/// The color of the text field's text when the cell's state is invalid.
+@property (assign, nonatomic) CGFloat floatingLabelYPadding;
+
+@property (strong, nonatomic) UIActivityIndicatorView *activityIndicatorView;
+
+/// A value indicating whether or not the cell shows a checkmark when valid. Default is YES.
+@property (assign, nonatomic) BOOL showsCheckmarkWhenValid;
+
+/// A value indicating whether or not the cell displays its validation state while being edited. Default is NO.
+@property (assign, nonatomic) BOOL showsValidationWhileEditing;
+
+/// The block called when the text field's text begins editing.
+@property (copy, nonatomic) void (^didBeginEditingBlock)(BZGTextViewCell *cell, NSString *text);
+
+/**
+ * The block called before the text field's text changes.
+ * The block's newText parameter will be the text field's text after changing. Return NO if the text shouldn't change.
+ */
+@property (copy, nonatomic) BOOL (^shouldChangeTextBlock)(BZGTextViewCell *cell, NSString *newText);
+
+/// The block called when the text field's text ends editing.
+@property (copy, nonatomic) void (^didEndEditingBlock)(BZGTextViewCell *cell, NSString *text);
+
+/// The block called before the text field returns. Return NO if the text field shouldn't return.
+@property (copy, nonatomic) BOOL (^shouldReturnBlock)(BZGTextViewCell *cell, NSString *text);
+
+/// A custom view used to replace the valid checkmark
+@property (strong, nonatomic) UIImage *accessoryImage UI_APPEARANCE_SELECTOR;
+
+/// Use float field instead of label/textfield
+- (id)initWithFloatField;
+
+/// Set the textField text
+-(void)setText:(NSString *)text;
+
+- (CGFloat)cellHeight;
+
+/**
+ * Returns the parent BZGTextFieldCell for the given text field. If no cell is found, returns nil.
+ *
+ * @param textField A UITextField instance that may or may not belong to this BZGTextFieldCell instance.
+ */
++ (BZGTextViewCell *)parentCellForTextField:(UITextView *)textField;
+
+@end
---
+++ BZGFormViewController/BZGRichTextViewCell.m
@@ -0,0 +1,255 @@
+//
+//  BZGTextFieldCell.m
+//
+//  https://github.com/benzguo/BZGFormViewController
+//
+
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <libextobjc/EXTScope.h>
+
+#import "BZGRichTextViewCell.h"
+#import "BZGInfoCell.h"
+#import "Constants.h"
+
+@interface BZGRichTextViewCell () <UITextViewDelegate>
+/// Whether to use a float textfield or label and textfield
+@property (assign, nonatomic) BOOL isFloatField;
+@end
+
+@implementation BZGRichTextViewCell
+
+- (id)initWithFloatField
+{
+    self = [super init];
+    if (self) {
+        self.isFloatField = YES;
+        [self setup];
+    }
+    return self;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.isFloatField = NO;
+        [self setup];
+    }
+    return self;
+}
+
+-(void)setup
+{
+    self.showsCheckmarkWhenValid = YES;
+    self.showsValidationWhileEditing = NO;
+    self.infoCell = [[BZGInfoCell alloc] init];
+
+    [self configureActivityIndicatorView];
+    
+    if (!_isFloatField) {
+        [self configureLabel];
+    }
+    [self configureTextField];
+    
+    [self configureTap];
+    [self configureBindings];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(textFieldTextDidEndEditing:)
+                                                 name:UITextViewTextDidEndEditingNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(textFieldTextDidChange:)
+                                                 name:UITextViewTextDidChangeNotification
+                                               object:nil];
+}
+
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    CGRect textViewFrame = self.textField.frame;
+    textViewFrame.origin.x = self.separatorInset.left;
+    textViewFrame.origin.y = self.textLabel.frame.origin.y + self.textLabel.frame.size.height;
+    textViewFrame.size.width = self.contentView.bounds.size.width - self.separatorInset.left - self.separatorInset.right;
+    CGSize textViewSize = [self.textField sizeThatFits:CGSizeMake(self.textField.frame.size.width, FLT_MAX)];
+    CGFloat height = ceilf(textViewSize.height);
+    height =  height < BZG_TEXTVIEW_MIN_HEIGHT ? BZG_TEXTVIEW_MIN_HEIGHT: height;
+    textViewFrame.size.height =  height;
+    if (![self.textLabel.text length])
+    {
+        textViewFrame.origin.y = self.textLabel.frame.origin.y;
+    }
+    self.textField.frame = textViewFrame;
+    
+    textViewFrame.origin.x += 5;
+    textViewFrame.size.width -= 5;
+    self.detailTextLabel.frame = textViewFrame;
+    
+    CGRect contentViewFrame = self.contentView.frame;
+    contentViewFrame.size.height = self.textField.frame.origin.y + self.textField.frame.size.height + 15;
+    self.contentView.frame = contentViewFrame;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)configureTextField
+{
+    CGFloat textFieldY = 0;
+    if (self.isFloatField) {
+        CGFloat textFieldX = self.separatorInset.left;
+        CGRect textFieldFrame = CGRectMake(textFieldX,
+                                           textFieldY,
+                                           self.bounds.size.width - textFieldX - self.activityIndicatorView.frame.size.width,
+                                           self.bounds.size.height);
+        self.textField = [[JVFloatLabeledTextView alloc] initWithFrame:textFieldFrame];
+        ((JVFloatLabeledTextView*)self.textField).floatingLabelYPadding = 5;
+        ((JVFloatLabeledTextView*)self.textField).scrollEnabled = NO;
+    }
+    else{
+        CGFloat textFieldX = self.bounds.size.width * 0.35;
+        CGRect textFieldFrame = CGRectMake(textFieldX,
+                                           textFieldY,
+                                           self.bounds.size.width - textFieldX - self.activityIndicatorView.frame.size.width,
+                                           self.bounds.size.height);
+        self.textField = [[UITextView alloc] initWithFrame:textFieldFrame];
+    }
+    
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeSentences;
+    self.textFieldNormalColor = BZG_TEXTFIELD_NORMAL_COLOR;
+    self.textFieldInvalidColor = BZG_TEXTFIELD_INVALID_COLOR;
+    self.textField.font = BZG_TEXTFIELD_FONT;
+    self.textField.backgroundColor = [UIColor clearColor];
+    self.textField.delegate = self;
+
+    [self addSubview:self.textField];
+}
+
+- (void)configureLabel
+{
+    CGFloat labelX = self.separatorInset.left;
+    CGRect labelFrame = CGRectMake(labelX,
+                                   0,
+                                   self.textField.frame.origin.x - labelX,
+                                   self.bounds.size.height);
+    self.label = [[UILabel alloc] initWithFrame:labelFrame];
+    self.label.font = BZG_TEXTFIELD_LABEL_FONT;
+    self.label.textColor = BZG_TEXTFIELD_LABEL_COLOR;
+    self.label.backgroundColor = [UIColor clearColor];
+    [self addSubview:self.label];
+}
+
+- (void)configureActivityIndicatorView
+{
+    CGFloat activityIndicatorWidth = self.bounds.size.height*0.7;
+    CGRect activityIndicatorFrame = CGRectMake(self.bounds.size.width - activityIndicatorWidth,
+                                               0,
+                                               activityIndicatorWidth,
+                                               self.bounds.size.height);
+    self.activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    [self.activityIndicatorView setFrame:activityIndicatorFrame];
+    self.activityIndicatorView.hidesWhenStopped = NO;
+    self.activityIndicatorView.hidden = YES;
+    [self addSubview:self.activityIndicatorView];
+}
+
+- (void)configureTap {
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(becomeFirstResponder)];
+    [self.contentView addGestureRecognizer:tap];
+}
+
+- (void)configureBindings
+{
+   
+
+
+
+}
+
++ (BZGTextViewCell *)parentCellForTextField:(UITextView *)textField
+{
+    UIView *view = textField;
+    while ((view = view.superview)) {
+        if ([view isKindOfClass:[BZGFormCell class]]) break;
+    }
+    return (BZGTextViewCell *)view;
+}
+
+- (void)setShowsCheckmarkWhenValid:(BOOL)showsCheckmarkWhenValid
+{
+    _showsCheckmarkWhenValid = showsCheckmarkWhenValid;
+    // Force RACObserve to trigger
+    self.validationState = self.validationState;
+}
+
+-(void)setFloatingLabelYPadding:(CGFloat)floatingLabelYPadding
+{
+    if ([self.textField isKindOfClass:[JVFloatLabeledTextView class]]) {
+        ((JVFloatLabeledTextView*)self.textField).floatingLabelYPadding = floatingLabelYPadding;
+    }
+}
+
+- (void)setAccessoryViewImage:(UIImage *)image {
+    self.accessoryView = [[UIImageView alloc] initWithImage:image];
+    self.accessoryView.frame = CGRectMake(0, 0, 24, 24);
+}
+
+#pragma mark - UITextField notification selectors
+// I'm using these notifications to flush the validation state signal.
+// It works, but seems hacky. Is there a better way?
+
+- (void)textFieldTextDidChange:(NSNotification *)notification
+{
+    UITextField *textField = (UITextField *)notification.object;
+    if ([textField isEqual:self.textField]) {
+        self.validationState = self.validationState;
+
+    }
+}
+
+-(void)setText:(NSString *)text
+{
+    self.textField.text = text;
+
+    if ([self.textField.delegate respondsToSelector:@selector(textViewDidChange:)]) {
+        [self.textField.delegate textViewDidChange:self.textField];
+    }
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:UITextFieldTextDidChangeNotification object:self.textField];
+}
+
+- (void)textFieldTextDidEndEditing:(NSNotification *)notification
+{
+    UITextField *textField = (UITextField *)notification.object;
+    if ([textField isEqual:self.textField]) {
+        self.validationState = self.validationState;
+    }
+}
+
+- (BOOL)becomeFirstResponder
+{
+    return [self.textField becomeFirstResponder];
+}
+
+- (BOOL)resignFirstResponder
+{
+    return [self.textField resignFirstResponder];
+}
+
+- (CGFloat)cellHeight
+{
+    CGSize textViewSize = [self.textField sizeThatFits:CGSizeMake(self.textField.frame.size.width, FLT_MAX)];
+    CGFloat height = ceilf(textViewSize.height); /*FXFormFieldPaddingTop + FXFormFieldPaddingBottom*/;
+    return height < BZG_TEXTVIEW_MIN_HEIGHT ? BZG_TEXTVIEW_MIN_HEIGHT: height;
+}
+
+-(BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+@end
