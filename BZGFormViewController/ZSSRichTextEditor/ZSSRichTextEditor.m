a3613f23939715d0d72c7a2c9ec495c4f98beaf9
Seth Sandler <cerupcat@gmail.com>
2015-08-19 16:38:18 -0700

    comment out rich text nslog

--- BZGFormViewController/ZSSRichTextEditor/ZSSRichTextEditor.m
+++ BZGFormViewController/ZSSRichTextEditor/ZSSRichTextEditor.m
@@ -1015,7 +1015,7 @@ static Class hackishFixClass = Nil;
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
 
     NSString *urlString = [[request URL] absoluteString];
-    NSLog(@"web request %@", urlString);
+//    NSLog(@"web request %@", urlString);
     if (navigationType == UIWebViewNavigationTypeLinkClicked) {
         return NO;
     } else if ([urlString rangeOfString:@"callback://0/"].location != NSNotFound) {
